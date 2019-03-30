b0013bab4485e3d0997bd2c80e9b98ce95f1db67
kamakshiSK <kamakshi.raibagkar@gmail.com>
2019-03-30 00:59:43 -0500

    Completed web-ceasar

--- main.py
+++ main.py
@@ -4,50 +4,48 @@ from ceasar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-header = '''
+content = '''
 <!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
-    <body>
-        '''
 
-content = '''
+    <body>
+
       <form action="/" method="POST">
         <label> Rotate by
             <input type = "text" name = "rot" value = "0"/><br><br>
         </label>
         
         <textarea name="text" rows="5" cols="50">
+        {text_to_show}
         </textarea> <br><br>
 
         <input type="submit" name= "submit" value = "Submit Query" />
       </form>
-      '''
 
-footer='''
     </body>
 </html>
 '''
 
 @app.route("/")
 def index():
-    return header + content + footer
+    return content.format(text_to_show = "")
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -56,11 +54,8 @@ def encrypt():
 
     changed_text = rotate_string(text, rot)
 
-    form = '<h1>' + changed_text + '</h1>'
+    #form = '<h1>' + changed_text + '</h1>'
 
-    return form
-
-
-
+    return content.format(text_to_show = changed_text)
 
 app.run()
